87bfce8d1186ee8164816deb846f980c18ccd327
GPE <owlprojectz@gmail.com>
2013-08-07 17:47:27 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/htc/marvel/device_marvel.mk)
 
-## Device identifier. This must come after all inclusions
+## Device identifier.
 PRODUCT_DEVICE := marvel
 PRODUCT_NAME := cm_marvel
 PRODUCT_BRAND := htc_europe
@@ -17,8 +17,8 @@ PRODUCT_CHARACTERISTICS := phone
 CM_BUILDTYPE := [OWLPROJECT]
 BUILD_ID := JWR66V
 
-# Set build fingerprint / ID / Product Name ect.
+# Set build fingerprint.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_DISPLAY_ID=JWR66V \
     BUILD_FINGERPRINT=htc/marvel/JWR66V/ BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) \
-    PRIVATE_BUILD_DESC=owlproject/htc/marvel:4.3.0/JWR66V/$(BUILD_ID)/v0.1:user/release-keys
+    PRIVATE_BUILD_DESC=gpe/htc/marvel:4.3.0/JWR66V/$(BUILD_ID)/v0.1:user/release-keys
